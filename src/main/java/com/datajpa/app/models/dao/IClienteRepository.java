9a34471d75b19f1feb41ebe23d04447521c054af
wvill <wendyvillazana@gmail.com>
2022-07-15 12:11:43 -0500

    backend basico

--- src/main/java/com/datajpa/app/models/dao/IClienteRepository.java
+++ src/main/java/com/datajpa/app/models/dao/IClienteRepository.java
@@ -1,7 +1,6 @@
 package com.datajpa.app.models.dao;
 
 import com.datajpa.app.models.entity.Cliente;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IClienteRepository extends JpaRepository <Cliente,Long> {
